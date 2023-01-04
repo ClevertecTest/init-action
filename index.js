50e550c685e91d85a57a74d0b39dccf61b0f8c81
Vanya Korobeynikov <INKorobeynikov@alfabank.ru>
2023-01-04 12:02:42 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,52 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+const main = async () => {
+    try {
+        /**
+         * We need to fetch all the inputs that were provided to our action
+         * and store them in variables for us to use.
+         **/
+        const owner = core.getInput('owner', { required: true });
+        const repo = core.getInput('repo', { required: true });
+        const pr_number = core.getInput('pr_number', { required: true });
+        const token = core.getInput('token', { required: true });
+
+        /**
+         * Now we need to create an instance of Octokit which will use to call
+         * GitHub's REST API endpoints.
+         * We will pass the token as an argument to the constructor. This token
+         * will be used to authenticate our requests.
+         * You can find all the information about how to use Octokit here:
+         * https://octokit.github.io/rest.js/v18
+         **/
+        const octokit = new github.getOctokit(token);
+
+        await octokit.rest.repos.addCollaborator({
+            owner,
+            repo,
+            username: 'Gaurrus'
+        });
+        /**
+         * Create a comment on the PR with the information we compiled from the
+         * list of changed files.
+         */
+        //     await octokit.rest.issues.createComment({
+        //         owner,
+        //         repo,
+        //         issue_number: pr_number,
+        //         body: `
+        //     Pull Request #${pr_number} has been updated with: \n
+        //     - ${diffData.changes} changes \n
+        //     - ${diffData.additions} additions \n
+        //     - ${diffData.deletions} deletions \n
+        //   `
+        //     });
+
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+}
+
+// Call the main function to run the action
+main();
